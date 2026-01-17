52411cc992d6b3b679d525053339aa5626686f18
Nathan Yang <nathanyang.w@outlook.com>
2026-01-17 20:35:57 +0100

    Put packages constants in another file

--- src/scripts/package_installer.py
+++ src/scripts/package_installer.py
@@ -7,18 +7,9 @@ Notes:
 """
 
 import os
+from packages import *
 
-# Constants
-PACKAGE_MANAGERS: dict[str, str] = {
-        "windows": "winget",
-        "linux": "apt",
-        "macos": "brew",
-}
-WINDOWS_PACKAGES: list[str] = ["Vim.Vim", "Git.Git", "Python.Python.3"]
-LINUX_PACKAGES: list[str] = ["vim", "git", "python3"]
-
-# Functions
-def install_packages(packages: list[str], package_manager: str) -> int:
+def install_packages(pkg_manager: dict) -> int:
     """
     Install packages depending on the package_manager.
 
@@ -26,20 +17,22 @@ def install_packages(packages: list[str], package_manager: str) -> int:
 
     Returns:
     """
-    if package_manager != "winget":
-        os.system(f"sudo apt update && sudo apt full-upgrade")
-        for package in packages:
-            os.system(f"sudo {package_manager} install {package} -y")
-        return 0
-    else:
-        os.system(f"winget upgrade --id Microsoft.AppInstaller")
-        for package in packages:
-            os.system("winget install --id {package} -e")
-        return 0
-    return 1
+    package_managers: list[str] = list(pkg_manager.keys())
+    for package_manager in package_managers:
+        if package_manager != "winget":
+            os.system(f"sudo apt update && sudo apt full-upgrade")
+            for package in packages:
+                os.system(f"sudo {package_manager} install {package} -y")
+            return 0
+        else:
+            os.system(f"winget upgrade --id Microsoft.AppInstaller")
+            for package in packages:
+                os.system("winget install --id {package} -e")
+            return 0
+        return 1
 
 
 if __name__ == "__main__":
-    install_packages(LINUX_PACKAGES, PACKAGE_MANAGERS["linux"])  # OK
+    install_packages(PACKAGE_MANAGERS)  # OK
 
     print("All tests passed")
