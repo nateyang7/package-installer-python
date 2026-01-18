1cbf8e2c618c1130efe7a80d937d56dbc59ea44c
Nathan Yang <nathanyang.w@outlook.com>
2026-01-18 18:01:24 +0100

    Add prototype

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 ## Introduction
 Packages installer automated installation of packages on Windows and Unix.
 
+## Table of contents
+- [[##Installation]]
+- [[##Usage]]
+- [[##Features]]
+
+## Installation
+Download the repository.
+
+## Usage
+Execute the main file with `./main.py`.
+
+## Features
+- [X] Install packages for Windows
+- [/] Install packages for Linux Ubuntu / Debian
+- [X] Install packages for MacOS
---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+TO-DO:
+    - [] 
+"""
+import os, platform
+
+def install_packages() -> int:
+    """
+    Install packages depending on the operation system.
+
+    Returns:
+        int: 0 if no problems else 1.
+    """
+    if platform.system() == "Windows":
+        ...
+        
+    elif operating_system == LINUX:
+        ...
+    else:
+        return 1
+
+def main() -> int:
+    install_packages()
+    return 0
+
+
+main()
