c61865e67870706df3691d429a892e107645f4c5
nateyang7 <nathanyang.w@outlook.com>
2026-03-04 04:13:31 +0100

    Fix return code by switching with string value for function install_package

--- src/package_installer/package_installer.py
+++ src/package_installer/package_installer.py
@@ -4,7 +4,29 @@ import platform
 import subprocess
 
 INSTALLATION_ERROR: int = 0
+PACKAGE_INSTALLATION_SUCCESS: str = ""
+PACKAGE_ALREADY_INSTALLED: str = "Found"
 ERROR_PACKAGE_NOT_FOUND: str = "No package found matching input criteria."
+
+def display_packages_status(packages: dict) -> None:
+    """
+    Display status for each package contained in a dict.
+
+    Args:
+        packages (dict): Packages dict.
+    
+    Returns:
+        None.
+    """
+    name_length: int = max(len(package_name) for package_name in packages.keys())
+
+    print("=== PACKAGES ===")
+    for package in packages:
+        if len(package) == name_length:
+            print(f"{package} => {packages[package]}")
+        else:
+            print(f"{package + ' ' * (name_length - len(package))} => {packages[package]}")
+
 
 def install_packages(packages: list[str]) -> None:
     """
@@ -19,7 +41,7 @@ def install_packages(packages: list[str]) -> None:
     Examples:
         >>> install_packages(["Git.Git", "Python.Python3.10"])
     """
-    installed_packages: dict[str, str] = {package: '?' for package in packages}
+    installed_packages: dict[str, str] = {package: '' for package in packages}
 
     if platform.system() == "Windows":
         for package in packages:
@@ -28,16 +50,15 @@ def install_packages(packages: list[str]) -> None:
                 capture_output=True,
                 text=True
             )
-            if installation.returncode != INSTALLATION_ERROR:
+            if PACKAGE_INSTALLATION_SUCCESS in installation.stdout.strip().split():
                 installed_packages[package] = "Installed"
+            elif PACKAGE_ALREADY_INSTALLED in installation.stdout.strip().split():
+                installed_packages[package] = "Already Installed"
             else:
-                installed_packages[package] = installation.returncode
+                installed_packages[package] = "Package not found"
 
     # LOGS
-    print("=== PACKAGES ===")
-    for package in installed_packages:
-        print(f"{package}: {installed_packages[package]}")
-    print()
+    display_packages_status(installed_packages)
 
 
 def remove_packages(packages: list[str]) -> None:
@@ -55,9 +76,9 @@ def remove_packages(packages: list[str]) -> None:
 
 if __name__ == "__main__":
     test_success_packages: list[str] = ["Git.Git", "Mozilla.Firefox", "potato"]
-    test_error_packages: list[str] = ["apple", "potato", "banana"]
+    #test_error_packages: list[str] = ["apple", "potato", "banana"]
 
     install_packages(test_success_packages)
-    install_packages(test_error_packages)
+    #install_packages(test_error_packages)  # => Verify return code
 
     print("All tests passed!")
