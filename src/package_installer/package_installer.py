bad84b741ad837b2b05c5805d286af4b6f8b1c1d
nateyang7 <nathanyang.w@outlook.com>
2026-03-04 06:59:28 +0100

    Finish windows support

--- src/package_installer/package_installer.py
+++ src/package_installer/package_installer.py
@@ -3,9 +3,16 @@
 import platform
 import subprocess
 
+# Package manger
+WINDOWS_PKGM: str = "winget"
+LINUX_PKGM: str = "apt"
+MACOS_PKGM: str = "brew"
+
+# Installation / Removal status
 INSTALLATION_ERROR: int = 0
 PACKAGE_INSTALLATION_SUCCESS: str = "Successfully installed"
 PACKAGE_ALREADY_INSTALLED: str = "Found"
+PACKAGE_REMOVAL_SUCCESS: str = "Successfully uninstalled"
 
 def display_packages_status(packages: dict[str, str]) -> None:
     """
@@ -23,6 +30,7 @@ def display_packages_status(packages: dict[str, str]) -> None:
     for package in packages:
         package_name: str = package + ' ' * (name_length - len(package))
         print(f"{package_name} => {packages[package]}")
+    print()
 
 
 def get_packages(filepath: str) -> list[str]:
@@ -52,24 +60,25 @@ def install_packages(packages: list[str]) -> None:
     Examples:
         >>> install_packages(["Git.Git", "Python.Python3.10"])
     """
-    installed_packages: dict[str, str] = {package: '' for package in packages}
+    packages_status: dict[str, str] = {package: '' for package in packages}
 
+    print("INSTALLING PACKAGES...")
     if platform.system() == "Windows":
         for package in packages:
             installation: subprocess.CompletedProcess[bytes] = subprocess.run(
-                ["winget", "install", "--id", package, "-e"],
+                [WINDOWS_PKGM, "install", "--id", package, "-e"],
                 capture_output=True,
                 text=True
             )
-            if installation.stdout.strip().find(PACKAGE_INSTALLATION_SUCCESS):
-                installed_packages[package] = "Installed"
+            if installation.stdout.strip().find(PACKAGE_INSTALLATION_SUCCESS) != -1:
+                packages_status[package] = "Installed"
             elif PACKAGE_ALREADY_INSTALLED in installation.stdout.strip().split():
-                installed_packages[package] = "Already Installed"
+                packages_status[package] = "Already Installed"
             else:
-                installed_packages[package] = "Package not found"
+                packages_status[package] = "Package not found"
 
     # LOGS
-    display_packages_status(installed_packages)
+    display_packages_status(packages_status)
 
 
 def remove_packages(packages: list[str]) -> None:
@@ -82,14 +91,32 @@ def remove_packages(packages: list[str]) -> None:
     Returns:
         None.
     """
-    ...
+    packages_status: dict[str, str] = {package: '' for package in packages}
+
+    print("REMOVING PACKAGES...")
+    if platform.system() == "Windows":
+        for package in packages:
+            removal: subprocess.CompletedProcess[bytes] = subprocess.run(
+                [WINDOWS_PKGM, "uninstall", "--id", package],
+                capture_output=True,
+                text=True
+            )
+
+            if removal.stdout.strip().find(PACKAGE_REMOVAL_SUCCESS) != -1:
+                packages_status[package] = "UNINSTALLED"
+            else:
+                packages_status[package] = removal.stderr
+
+    display_packages_status(packages_status)
 
 
 if __name__ == "__main__":
-    # Ensure that git is installed before testing
-    test_windows_packages: list[str] = ["Notepad++.Notepad++", "Git.Git", "python"]
+    # MUST FOLLOW -> [NOT_INSTALLED, INSTALLED, UNKNOWN]
+    TEST_WINDOWS_PACKAGES: list[str] = ["Notepad++.Notepad++", "Git.Git", "pyton"]
+    TEST_LINUX_PACKAGES: list[str] = ["gedit", "git", "pyton"]
 
-    #remove_packages()
-    install_packages(test_windows_packages)
+    # WINDOWS: Ensure that you are on a windows machine before testing
+    install_packages(TEST_WINDOWS_PACKAGES)
+    remove_packages([TEST_WINDOWS_PACKAGES[0], ])
 
     print("All tests passed!")
